4d707057eee8bbfaf571200f11fea4623e942706
Anatoliy <canatolij@list.ru>
2024-04-22 21:11:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <a className='link-wb'
-          href={'https://www.wildberries.ru/brands/310616779-coloco'}
-          rel ={'nofollow noreferrer'}
-      target={'_blank'}
-      >
-          магазин
-      </a>
-        <a className='link-tg'
-           href={'https://t.me/WB_COLOCO_BOT'}
-           rel ={'nofollow noreferrer'}
-           target={'_blank'}
-        >
-          телеграмм бот
-      </a>
+        <header className={'header'}>
+            <p className={'logo'}>COLOCO</p>
+        </header>
+        <div className={'zeroblock'} />
+        <main className={'main'}>
+            <a className='link-wb'
+               href={'https://www.wildberries.ru/brands/310616779-coloco'}
+               rel ={'nofollow noreferrer'}
+               target={'_blank'}
+            >
+                товары магазина
+            </a>
+            <a className='link-tg'
+               href={'https://docs.google.com/forms/d/e/1FAIpQLSeKTpm_Y9LuCckIREGIgRxGF7Ug2VkZEWQTPvArbSSTG_CYJg/viewform?usp=sf_link'}
+               rel ={'nofollow noreferrer'}
+               target={'_blank'}
+            >
+                служба заботы
+            </a>
+        </main>
+     <footer>
+
+     </footer>
     </div>
   );
 }
