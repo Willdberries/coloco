3a8e6bafaa46d90dfbf8b6ecd9ab7b5c47f8fb4d
Anatoliy <canatolij@list.ru>
2024-05-04 13:48:19 +0300

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './App.css';
 
 function App() {
+    const clickButton = (event:string) =>{
+        (window as any)?.gtag('event', `clickButtonOpen${event}`, { event_name: event })
+    }
+
+    useEffect(()=>{
+        (window as any)?.gtag('event', 'open', { event_name: 'openPage' })},[])
+
   return (
     <div className="App">
         <header className={'header'}>
@@ -13,6 +20,7 @@ function App() {
                href={'https://www.wildberries.ru/brands/310616779-coloco'}
                rel ={'nofollow noreferrer'}
                target={'_blank'}
+               onClick={()=>clickButton('WB')}
             >
                 товары магазина
             </a>
@@ -20,6 +28,7 @@ function App() {
                href={'https://docs.google.com/forms/d/e/1FAIpQLSeKTpm_Y9LuCckIREGIgRxGF7Ug2VkZEWQTPvArbSSTG_CYJg/viewform?usp=sf_link'}
                rel ={'nofollow noreferrer'}
                target={'_blank'}
+               onClick={()=>clickButton('Help')}
             >
                 служба заботы
             </a>
