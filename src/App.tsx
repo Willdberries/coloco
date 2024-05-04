b14227af99011e685a0acceacd5273f929b24846
Anatoliy <canatolij@list.ru>
2024-05-04 18:39:10 +0300

    add link feedback

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,59 @@ import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
-    const clickButton = (event:string) =>{
-        (window as any)?.gtag('event', `clickButtonOpen${event}`, { event_name: event })
+    const clickButton = (event: string) => {
+        (window as any)?.gtag('event', `clickButtonOpen${event}`, {event_name: event})
     }
 
-    useEffect(()=>{
-        (window as any)?.gtag('event', 'open', { event_name: 'openPage' })},[])
+    useEffect(() => {
+        (window as any)?.gtag('event', 'open', {event_name: 'openPage'})
+    }, [])
 
-  return (
-    <div className="App">
-        <header className={'header'}>
-            <p className={'logo'}>COLOCO</p>
-        </header>
-        <main className={'main'}>
-            <a className='link-wb'
-               href={'https://www.wildberries.ru/brands/310616779-coloco'}
-               rel ={'nofollow noreferrer'}
-               target={'_blank'}
-               onClick={()=>clickButton('WB')}
-            >
-                товары магазина
-            </a>
-            <a className='link-tg'
-               href={'https://docs.google.com/forms/d/e/1FAIpQLSeKTpm_Y9LuCckIREGIgRxGF7Ug2VkZEWQTPvArbSSTG_CYJg/viewform?usp=sf_link'}
-               rel ={'nofollow noreferrer'}
-               target={'_blank'}
-               onClick={()=>clickButton('Help')}
-            >
-                служба заботы
-            </a>
-        </main>
-     <footer>
+    return (
+        <div className="App">
+            <header className={'header'}>
+                <p className={'logo'}>COLOCO</p>
+            </header>
+            <main className={'main'}>
+                <a className='link-wb'
+                   href={'https://www.wildberries.ru/brands/310616779-coloco'}
+                   rel={'nofollow noreferrer'}
+                   target={'_blank'}
+                   onClick={() => clickButton('WB')}
+                >
+                    товары магазина
+                </a>
+                <a className='link-tg'
+                   href={'https://docs.google.com/forms/d/e/1FAIpQLSeKTpm_Y9LuCckIREGIgRxGF7Ug2VkZEWQTPvArbSSTG_CYJg/viewform?usp=sf_link'}
+                   rel={'nofollow noreferrer'}
+                   target={'_blank'}
+                   onClick={() => clickButton('Help')}
+                >
+                    служба заботы
+                </a>
+            </main>
+            <footer>
+                <div className={'feedback-container'}>
+                    <p className={'feedback-text'}>оставить отзыв</p>
+                    <a className='feedback-link'
+                       href={'https://www.wildberries.ru/lk/myorders/archive'}
+                       rel={'nofollow noreferrer'}
+                       target={'_blank'}
+                       onClick={() => clickButton('Feedback')}
+                    >
+                        <ul className={'star-container'}>
+                            <li className={'star'}/>
+                            <li className={'star'}/>
+                            <li className={'star'}/>
+                            <li className={'star'}/>
+                            <li className={'star'}/>
+                        </ul>
+                    </a>
+                </div>
 
-     </footer>
-    </div>
-  );
+            </footer>
+        </div>
+    );
 }
 
 export default App;
